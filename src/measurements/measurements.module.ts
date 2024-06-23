f282330c8b2f0c15858c27e8a0c662042ad8bf7a
Matheus Costa <matheus.rodrigues@ligaeducacional.com.br>
2024-06-23 14:45:29 -0300

    feat: Added prisma module

--- src/measurements/measurements.module.ts
+++ src/measurements/measurements.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
 import { MeasurementsService } from './measurements.service';
 import { MeasurementsController } from './measurements.controller';
 
 @Module({
   controllers: [MeasurementsController],
-  providers: [MeasurementsService]
+  providers: [PrismaClient, MeasurementsService],
 })
 export class MeasurementsModule {}
